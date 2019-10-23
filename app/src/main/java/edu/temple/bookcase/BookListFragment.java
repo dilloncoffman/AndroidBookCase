8692ba67036c2a2096bba79eb859e1c765389ddb
Dillon Coffman <tug40394@temple.edu>
2019-10-23 16:47:52 -0400

    Laid out BookListFragment

--- app/src/main/java/edu/temple/bookcase/BookListFragment.java
+++ app/src/main/java/edu/temple/bookcase/BookListFragment.java
@@ -1,12 +1,14 @@
 package edu.temple.bookcase;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 
 /**
@@ -18,14 +20,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class BookListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ArrayList<String> books;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public final static String BOOKS_KEY = "books";
 
     private OnBookSelectedInterface fragmentParent;
 
@@ -37,26 +34,23 @@ public class BookListFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param books ArrayList<String> of book titles
      * @return A new instance of fragment BookListFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static BookListFragment newInstance(String param1, String param2) {
-        BookListFragment fragment = new BookListFragment();
+    public static BookListFragment newInstance(ArrayList<String> books) {
+        BookListFragment bookListFragment = new BookListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putStringArrayList(BOOKS_KEY, books);
+        bookListFragment.setArguments(args);
+        return bookListFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        Bundle args = getArguments();
+        if (args != null) {
+            books = args.getStringArrayList(BOOKS_KEY);
         }
     }
 
@@ -64,7 +58,14 @@ public class BookListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_list, container, false);
+        ListView listView = (ListView) inflater.inflate(R.layout.fragment_book_list, container, false);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                fragmentParent.bookSelected(position);
+            }
+        });
+        return listView;
     }
 
     @Override
