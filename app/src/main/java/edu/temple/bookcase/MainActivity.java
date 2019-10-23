9cfe31fcdd3705790b39178bb01165e1412c7acc
Dillon Coffman <tug40394@temple.edu>
2019-10-23 10:46:05 -0400

    Created string-array resource for books, created ArrayList to add strings to

--- app/src/main/java/edu/temple/bookcase/MainActivity.java
+++ app/src/main/java/edu/temple/bookcase/MainActivity.java
@@ -1,13 +1,29 @@
 package edu.temple.bookcase;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    List<String> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get books from books string-array resource
+        Resources res = getResources();
+        books = Arrays.asList(res.getStringArray(R.array.books));
+
+        for (String book : books){
+            Log.d("Book name: ", book);
+        }
     }
 }
