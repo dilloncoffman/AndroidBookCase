64921cea72de72c77aa5f86ff63983c3be280576
Dillon Coffman <tug40394@temple.edu>
2019-10-23 20:58:54 -0400

    All layouts now work with selecting a book from the BookListFragment and display its title in the BookDetailsFragment regardless of layout

--- app/src/main/java/edu/temple/bookcase/MainActivity.java
+++ app/src/main/java/edu/temple/bookcase/MainActivity.java
@@ -4,12 +4,12 @@ import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements BookListFragment.OnBookSelectedInterface {
+    BookDetailsFragment bookDetailsFragment;
     ArrayList<String> books = new ArrayList<>();
     boolean singlePane;
 
@@ -33,19 +33,32 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
             // pop it off stack if it is a BookDetailsFragment so it doesn't show over BookListFragment
             getSupportFragmentManager().popBackStack();
         }
-
-//        if (singlePane) {
-//            // use ViewPager for swiping through BookDetailsFragments
-//        }
-
-//        for (Object book : books){
-//            Log.d("Book name: ", (String) book);
-//        }
     }
 
     @Override
     public void bookSelected(int position) {
-        String bookTitle = books.get(position);
-        Log.d("Book selected", bookTitle);
+        String bookTitle = books.get(position); // Get bookTitle for the selected position
+
+        // Add bookTitle to bundle to be passed to the BookDetailsFragment
+        bookDetailsFragment = new BookDetailsFragment();
+        Bundle detailsBundle = new Bundle();
+        detailsBundle.putString(BookDetailsFragment.BOOK_TITLE_KEY, bookTitle);
+        bookDetailsFragment.setArguments(detailsBundle);
+
+        if (singlePane) {
+            // use ViewPager
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .addToBackStack(null) // allows us to hit back arrow and go back to last BookListFragment rather than going back to home screen and closing the app
+                    .replace(R.id.container_1, bookDetailsFragment)
+                    .commit();
+        } else {
+            // container_2 should always attach the BookDetailsFragment if not in singlePane
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .addToBackStack(null) // allows us to hit back arrow and go back to last BookListFragment rather than going back to home screen and closing the app
+                    .replace(R.id.container_2, bookDetailsFragment)
+                    .commit();
+        }
     }
 }
